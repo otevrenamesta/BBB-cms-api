af0bbe61d2c147c9c8fa53a133a578de7b26ab99
vencax <vencax@centrum.cz>
2021-05-05 15:57:54 +0200

    small fix

--- files.js
+++ files.js
@@ -19,8 +19,6 @@ async function listPages (filePath) {
   })
 }
 
-//   {path: "/", data: "index.yaml"},
-
 const scripts = [
   'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js',
