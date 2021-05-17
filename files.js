65d9247806626d71cbf2ac60f9b60c96504b9d37
vencax <vencax@centrum.cz>
2021-05-17 20:14:51 +0200

    url change

--- files.js
+++ files.js
@@ -29,7 +29,7 @@ const scripts = [
   'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.1.3/vue-router.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.0.0/js-yaml.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.1/marked.min.js',
-  'https://raw.githubusercontent.com/vencax/bbb-vue-web/master/dist/bbb-web.js'
+  'https://raw.githubusercontent.com/otevrenamesta/bbb-vue-web/master/dist/bbb-web.js'
 ]
 
 async function concatVendorScripts () {
