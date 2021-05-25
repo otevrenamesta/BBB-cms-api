10a66c3e8feb4d6579cc4d953606a80d43d54a19
vencax <vencax@centrum.cz>
2021-05-25 11:11:16 +0200

    fix of relative imports in scss

--- sass_render.js
+++ sass_render.js
@@ -1,10 +1,11 @@
 import fs from 'fs'
 import path from 'path'
 import sass from 'sass'
+const NODE_MODULES = path.join(__dirname, './node_modules/')
 
 export default async function buildStyle (webid, dataFolder) {
-  const styleMain = path.join(dataFolder, webid, '_service', 'style', 'custom.scss')
-  const includePaths = ['./node_modules/bootstrap/scss']
+  const styleMain = path.join(dataFolder, webid, '_service', 'style', 'custom.scss')  
+  const includePaths = [ NODE_MODULES ]
   const f = sass.renderSync({ file: styleMain, includePaths })
   return f.css
     // fs.writeFile(outFile, f.css, (err) => {
