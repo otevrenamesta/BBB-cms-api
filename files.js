b7c2b3cf9d0453171fc0aa1b7aacd204d76b49aa
vencax <vencax@centrum.cz>
2021-06-30 20:45:01 +0200

    vendor builder

--- files.js
+++ files.js
@@ -2,13 +2,9 @@ import readdirp from 'readdirp'
 import path from 'path'
 import fs from 'fs'
 import _ from 'underscore'
-import axios from 'axios'
 import yaml from 'yaml'
 
-export default {
-  listPages, listMetaInfo, concatVendorScripts, 
-  update, create, fileList, writeFile
-}
+export default { listPages, listMetaInfo, update, create, fileList, writeFile }
 
 async function listPages (filePath) {
   const files = await readdirp.promise(filePath, { fileFilter: '*.yaml' })
@@ -44,28 +40,6 @@ async function listMetaInfo (filePath) {
     })
   }))
   return JSON.stringify(data)
-}
-
-const scripts = [
-  'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/cs.min.js',
-  'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.1.2/vuex.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.1.3/vue-router.min.js',
-  'https://unpkg.com/vue-meta@2.4.0/dist/vue-meta.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/js-yaml/4.0.0/js-yaml.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/marked/2.0.1/marked.min.js',
-  'https://raw.githubusercontent.com/otevrenamesta/bbb-vue-web/master/dist/bbb-web.js'
-]
-
-async function concatVendorScripts () {
-  const reqs = await Promise.all(scripts.map(i => axios.get(i)))
-  const s = _.reduce(reqs, (acc, i) => {
-    return acc + '\n\n' + i.data
-  }, '')
-  return s
 }
 
 // async function renderIndex (hostname) {
