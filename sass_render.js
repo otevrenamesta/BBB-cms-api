43da5f684322c0a5c7441e0b0bc0b114f4117c6e
vencax <vencax@centrum.cz>
2021-07-15 13:17:49 +0200

    sass error rendered to css

--- sass_render.js
+++ sass_render.js
@@ -6,8 +6,12 @@ const NODE_MODULES = path.join(__dirname, './node_modules/')
 export default async function buildStyle (filePath) {
   const styleMain = path.join(filePath, '_service', 'style', 'custom.scss')  
   const includePaths = [ NODE_MODULES ]
-  const f = sass.renderSync({ file: styleMain, includePaths })
-  return f.css
+  try {
+    const f = sass.renderSync({ file: styleMain, includePaths })
+    return f.css
+  } catch (err) {
+    return err.toString()
+  }
     // fs.writeFile(outFile, f.css, (err) => {
     //   return err ? console.error(err) : bs.reload(outFile)
     // })
