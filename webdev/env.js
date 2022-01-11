858e37366792745726fb99720874427aa98f925e
vencax <vencax@centrum.cz>
2022-01-11 13:43:50 +0100

    update

--- webdev/env.js
+++ webdev/env.js
@@ -1,5 +1,5 @@
 import path from 'path'
 process.env.DOMAIN = 'testweb.cz'
-process.env.DATA_FOLDER = path.join(__dirname, '.data')
+process.env.WEBDATA_FOLDER = path.join(__dirname, '.data')
 process.env.SESSION_MOCK=3333
 process.env.SESSION_SERVICE = `http://localhost:${process.env.SESSION_MOCK}`
--- webdev/server.js
+++ webdev/server.js
@@ -1,33 +1,26 @@
 import express from 'express'
 import path from 'path'
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import InitAPI from '../index'
+import { migrateDB, init as init } from '../index'
 import Prepare from './prepare'
 
-async function init () {
+async function doInit () {
   const g = {}
   const app = express()
   await Prepare()
   const staticFolder = path.join(process.env.WEBDATA_FOLDER, process.env.DOMAIN)
   app.use('/data', express.static(staticFolder))
-  try {
-    const proxies = JSON.parse(process.env.PROXIES)
-    for (let i in proxies) {
-      app.use(i, createProxyMiddleware({ target: proxies[i], changeOrigin: true }))
-    }
-  } catch (_) {}
   app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
   // g.sessionSrvcMock = SessionServiceMock(process.env.SESSION_SERVICE_PORT, g)
   const host = process.env.HOST || '127.0.0.1'
   const port = process.env.PORT || 8080
-  InitAPI().then(apiapp => {
-    app.use('/api', apiapp)
-    g.server = app.listen(port, '127.0.0.1', (err) => {
-      if (err) throw err
-      console.log(`frodo do magic on ${host}:${port}`)
-    })
+  await migrateDB()
+  // const apiapp = init({})
+  // app.use('/api', apiapp)
+  g.server = app.listen(port, '127.0.0.1', (err) => {
+    if (err) throw err
+    console.log(`frodo do magic on ${host}:${port}`)
   })
 }
 
-init()
+doInit()
